a05ab624a65148aad733aeb52ff9f7ff63d53706
hzhu <hz@henryzhu.me>
2016-12-13 19:27:21 -0800

    added mergesort solution

--- src/mergesort/index.js
+++ src/mergesort/index.js
@@ -1,7 +1,38 @@
-export function merge() {
+export function merge(left, right, array) {
+  let l_index = 0
+  let r_index = 0
+  let a_index = 0
 
+  left.push(Infinity)
+  right.push(Infinity)
+
+  while (a_index < array.length) {
+    if (left[l_index] < right[r_index]) {
+      array[a_index] = left[l_index]
+      l_index++
+    } else {
+      array[a_index] = right[r_index]
+      r_index++
+    }
+
+    a_index++
+  }
+
+  return array
 }
 
-export function mergesort() {
+export function mergesort(array) {
+  if (array.length < 2) {
+    return array
+  }
 
+  const mid = Math.floor(array.length * 0.5)
+
+  const l = array.slice(0, mid)
+  const r = array.slice(mid, array.length)
+
+  mergesort(l)
+  mergesort(r)
+
+  return merge(l, r, array)
 }
