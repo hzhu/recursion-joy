9bfa5757a51f2f181a43560ba17145989ec7a055
hzhu <hz@henryzhu.me>
2016-12-29 15:25:19 -0800

    Updated test-bundler.js to include /problems in tests.

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,6 +1,8 @@
 // Include all .js files under `solutions`, except app.js.
 // This is for code coverage
 
-const context = require.context('../../solutions', true, /^^((?!(main)).)*\.js$/)
+const solutions = require.context('../../solutions', true, /^^((?!(main)).)*\.js$/)
+const problems = require.context('../../problems', true, /^^((?!(main)).)*\.js$/)
 
-context.keys().forEach(context)
+solutions.keys().forEach(solutions)
+problems.keys().forEach(problems)
