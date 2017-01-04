13b63d3626d92df2438575d7e7b335f88c6123a3
hzhu <hz@henryzhu.me>
2017-01-04 04:23:19 -0800

    updated get_elements_by_classname solution

--- solutions/08 - get_elements_by_classname/index.js
+++ solutions/08 - get_elements_by_classname/index.js
@@ -1,20 +1,18 @@
 function get_elements_by_classname(node, value) {
   let results = [];
+
   (function traverse(node) {
     // base case
     if (node.className.indexOf(value) > -1) {
       results.push(node)
     }
 
-    if (node.children.length === 0) {
-      return
-    } else {
-      Array.prototype.forEach.call(node.children, (child) => {
+    if (node.children.length > 0) {
+      [].forEach.call(node.children, (child) => {
         traverse(child)
       })
     }
-
-  })(node)
+  }(node))
 
   return results
 }
