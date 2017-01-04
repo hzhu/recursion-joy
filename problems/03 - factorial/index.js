b9ac10a6b6289ee2b879773805638844f828fd48
hzhu <hz@henryzhu.me>
2017-01-04 04:28:12 -0800

    Removed factorial and exponent from /problems

--- problems/03 - factorial/index.js
+++
@@ -1,7 +0,0 @@
-function factorial(n) {
-  if (n === 1) return 1
-
-  return n * factorial(n - 1)
-}
-
-export default factorial
--- problems/03 - factorial/index.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import factorial from './index.js'
-
-describe('factorial', () => {
-  it('should calculate the factorial of n', () => {
-    expect(factorial(5)).to.be.equal(120)
-  })
-})
